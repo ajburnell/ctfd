39c757d91e23cba308d53782e2c15eb51475681c
ajburnell <30587915+ajburnell@users.noreply.github.com>
2023-07-29 13:28:12 +1000

    Create service_pass.py

---
+++ service_pass.py
@@ -0,0 +1,16 @@
+import secrets
+import subprocess
+
+password_length = 16
+
+# Generate a vault password and the Redis / MariaDB suser passwords.
+vault_password = secrets.token_urlsafe(password_length)
+service_password = secrets.token_urlsafe(password_length)
+
+# Write the vault password to file to be used by Ansible
+f = open("vault_password", "w")
+f.write (vault_password)
+f.close()
+
+# Add the service password to a vault
+ansible_vault = subprocess.run(["ansible-vault", "encrypt_string", "--vault-password-file", "vault_password", service_password, "--name", "service_password", "--output", "host_vars/ctfd_vault.yml"])
