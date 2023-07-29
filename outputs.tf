f261fba719ed60e87f06eee5a46c1c84b7239ea9
ajburnell <30587915+ajburnell@users.noreply.github.com>
2023-07-29 15:15:31 +1000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "instance_conn_details" {
   value = "ssh -i ${var.private_key_filename} ctfd@${aws_instance.ctfd_server.public_ip}"
 }
+
+output "ansible_conn_details " {
+  value = terraform_data.ctfd_ansible.local-exec.command
+}
